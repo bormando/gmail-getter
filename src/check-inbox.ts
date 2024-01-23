eebd6e930392827a0ca3a8851b64e25ae7144361
Dmitrii Bormotov <squier7@gmail.com>
2024-01-23 13:09:02 +0400

    Refactor check inbox function Add single or multiple emails boolean option Replace set of arguments with params object Return an email, array of emails or null Improve error tracing

--- src/check-inbox.ts
+++ src/check-inbox.ts
@@ -1,45 +1,70 @@
-import {getEmailsList, EmailsList, Email, getEmail} from './api'
+import {fetchEmailsListByQuery, Email, fetchEmailById, Message} from './api'
+
+export type CheckInboxOptions = {
+  token: string
+  timeout?: number
+  step?: number
+  all?: boolean
+  query?: string
+}
 
 /**
  * Check Gmail inbox
- * @param {string} token OAuth Access token
- * @param {number} timeout Function timeout (ms)
- * @param {number} step Time between retries (ms)
- * @param {string} query Query that specifies search criteria (https://support.google.com/mail/answer/7190)
- * @returns {Promise<Email | null>} Email contents
- * @example const email = await checkInbox('ya01.a123456...', 15000, 2000, 'from:squier7 subject:Test!')
+ * @param {CheckInboxOptions} options - The Options object
+ * @param {string} options.token OAuth Access token
+ * @param {number} [options.timeout=15000] Function timeout (ms)
+ * @param {number} [options.step=1500] Time between retries (ms)
+ * @param {boolean} [options.all=false] Whether to find a single email or all mathing the query criteria
+ * @param {string} [options.query] Query that specifies search criteria (https://support.google.com/mail/answer/7190)
+ * @returns {Promise<Email | Email[] | null>} Email contents
+ * @example const email = await checkInbox({token: 'ya01.a123456...', query: 'from:squier7 subject:Test!'})
  */
-export const checkInbox = async (
-  token: string,
-  timeout: number = 15000,
-  step: number = 1500,
-  query?: string
-): Promise<Email | null> => {
-  if (!token) throw new Error('Access Token is missing!')
+export const checkInbox = async (options: CheckInboxOptions): Promise<Email | Email[] | null> => {
+  const {token, query} = options
+  const timeout = options.timeout || 15000
+  const step = options.step || 1500
+  const all = options.all || false
 
-  let email: Email | null = null
-  let emails: EmailsList | null = null
+  if (!token) {
+    throw new Error('Access token is missing!')
+  }
+
+  let emails: Email[] = []
+  let messages: Message[] | undefined
   let startTime = Date.now()
 
   while (Date.now() - startTime < timeout) {
     try {
-      emails = await getEmailsList(token, query)
+      messages = await fetchEmailsListByQuery(token, query)
     } catch (error) {
       console.log(error)
     }
 
-    if (emails) break
+    if (messages) break
 
     await new Promise(resolve => setTimeout(resolve, step))
   }
 
-  if (emails) {
-    try {
-      email = await getEmail(emails.messages![0].id, token)
-    } catch (error) {
-      console.log(error)
+  if (!messages) {
+    return null
+  }
+
+  if (messages.length === 0) {
+    return null
+  }
+
+  for (const message of messages) {
+    const email = await fetchEmailById(message.id, token)
+    emails.push(email)
+
+    if (all) {
+      await new Promise(resolve => setTimeout(resolve, step))
     }
   }
 
-  return email
+  if (!all) {
+    return emails[0]
+  }
+
+  return emails
 }
