fb94313293a08073eb58642d94d60c984c14c89d
Dmitrii Bormotov <squier7@gmail.com>
2024-07-28 00:45:06 +0400

    Update types

--- src/api/types/emails-list.ts
+++ src/api/types/emails-list.ts
@@ -1,6 +1,12 @@
-import {Message} from './message'
+// https://developers.google.com/gmail/api/reference/rest/v1/users.messages/list
+
+export type Message = {
+  id: string
+  threadId: string
+}
 
 export type EmailsList = {
-  messages?: Message[]
+  messages: Message[]
+  nextPageToken: string
   resultSizeEstimate: number
 }
