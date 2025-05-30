18aedcf0ed114d5059c1509c01dde97f7a4b6e54
Dmitrii Bormotov <squier7@gmail.com>
2025-05-30 21:20:52 +0200

    Add file error tracing to the refresh token generation script

--- src/get-token-script.ts
+++ src/get-token-script.ts
@@ -2,6 +2,7 @@
 import {authenticate} from '@google-cloud/local-auth'
 import {OAuth2Client} from 'google-auth-library'
 import path from 'path'
+import fs from 'fs'
 
 const script = async (): Promise<void> => {
   const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json')
@@ -12,13 +13,19 @@ const script = async (): Promise<void> => {
     \nINFO: This script is processing information from 'credentials.json' in your current folder."
   )
 
+  if (!fs.existsSync(CREDENTIALS_PATH)) {
+    console.error(
+      `credentials.json not found in ${process.cwd()}
+        \nPlease place credentials.json in the current directory`
+    )
+
+    return
+  }
+
   let client: OAuth2Client
 
   try {
-    client = await authenticate({
-      scopes: SCOPES,
-      keyfilePath: CREDENTIALS_PATH,
-    })
+    client = await authenticate({scopes: SCOPES, keyfilePath: CREDENTIALS_PATH})
   } catch (e) {
     throw new Error(`Authentication request has failed.\n${JSON.stringify(e, null, 2)}`)
   }
